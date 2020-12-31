88f8a2b7175a0276ee8857c84c59d03380319dd0
tumit <tumit@live.com>
2020-12-31 15:22:26 +0700

    fix: just return string

--- apps/bc3bot-api/src/app/app.controller.ts
+++ apps/bc3bot-api/src/app/app.controller.ts
@@ -2,20 +2,20 @@ import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { ChatRequest } from '@nxth-tumit/bc3bot-data';
+import { Observable } from 'rxjs';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
   getData() {
-    return this.appService.getData('สวย');
+    return this.appService.getData({ command: 'สวย' });
   }
 
   @Post()
-  @Render('index')
-  chat(@Body() req: ChatRequest) {
-    return this.appService.getData(req.command);
+  postChat(@Body() req: ChatRequest): Observable<string> {
+    return this.appService.getData(req);
   }
 
 }
--- apps/bc3bot-api/src/app/app.service.ts
+++ apps/bc3bot-api/src/app/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { ChatRequest, ChatResponse } from '@nxth-tumit/bc3bot-data';
 import { FaunadbService, query as q, Client, values } from 'nestjs-faunadb';
-
-export interface BotResponse {
-  data: [];
-}
+import { from, Observable, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Injectable()
 export class AppService {
@@ -14,11 +13,18 @@ export class AppService {
     this.client = this.fauna.getClient();
   }
 
-  async getData(trigger: string): Promise<BotResponse> {
+  getData(req: ChatRequest): Observable<string> {
+    return from(this.getFaunaDbData(req.command)).pipe(
+      filter( r => r?.data?.length > 0),
+      map(r => (r?.data[0]))
+    );
+  }
+
+  async getFaunaDbData(trigger: string): Promise<{ data: []}> {
     const query = q.Paginate(
       q.Match(q.Index('responses_by_triggers'), trigger)
     );
-    const result = await this.client.query<BotResponse>(query);
+    const result = await this.client.query<{ data: []}>(query);
     return result;
   }
 
